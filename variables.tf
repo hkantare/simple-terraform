238e86cfbb236fd6e5196e3526b9135570e53f85
HARINI KANTAREDDY <hkantare@in.ibm.com>
2023-09-29 14:39:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 /*variable "ibmcloud_api_key" {
   description = "IBM Cloud Platform API Key"
-}*/
+}
 
 variable "resource_group_name" {
   description = "Where to create resources"
   default = "Default"
-}
+}*/
 
 variable "vpc_name" {
 }
