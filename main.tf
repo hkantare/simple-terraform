f70d22b96a3766c698cb1e2b195b491fa586b724
HARINI KANTAREDDY <hkantare@in.ibm.com>
2024-04-05 21:27:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,11 @@ resource "ibm_is_vpc" "vpc" {
   tags = ["tag1","tag2"]
 }
 
+import {
+  to = ibm_is_vpc.vpc
+  id = "r006-41401283-4ff3-456a-9b9b-e12b21d40b3a"
+}
+
 
 resource "ibm_is_vpc" "vpc1" {
   name           = "${var.vpc_name}1"
