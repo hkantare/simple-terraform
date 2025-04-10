cdb1f43ed479e882bfe2d345bc16d602435226b1
HARINI KANTAREDDY <hkantare@in.ibm.com>
2025-04-10 12:38:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,19 @@ variable "resource_group_name" {
   description = "Where to create resources"
   default = "Default"
 }
+
+
+terraform {
+  //required_version = "~> 1.1"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.43.0"
+    }
+  }
+}
+
+provider "ibm" {
+  # Configuration options
+  visibility = "private"
+}
