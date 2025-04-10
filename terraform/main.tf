74eacf97c1990512a45551e07b414faf40e5e97e
HARINI KANTAREDDY <hkantare@in.ibm.com>
2025-04-10 12:11:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
 data "ibm_resource_group" "group" {
   name = "${var.resource_group_name}"
 }
+
+variable "resource_group_name" {
+  description = "Where to create resources"
+  default = "Default"
+}
